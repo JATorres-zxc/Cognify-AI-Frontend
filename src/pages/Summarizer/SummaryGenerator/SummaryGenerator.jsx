ad11d4806cf1629b2ffe8f89e44243cf4115a08e
Li~chan <137862600+ChiakiLi-chan@users.noreply.github.com>
2025-06-02 00:22:35 +0800

    created summarizer page

---
+++ src/pages/Summarizer/SummaryGenerator/SummaryGenerator.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import Header from '../../../components/common/Header/Header';
+import Footer from '../../../components/common/Footer/Footer';
+import './SummaryGenerator.css';
+
+const SummaryGenerator = () => {
+    return (
+        <div className="home">
+            <div>
+            <Header />
+            </div>
+            <div className='main'>
+                <div className='main-header'>
+                    <h1>Summary Generator</h1>
+                </div>
+                <div className='line'></div>
+
+                <div className='btn-container'>
+                    <button className='upload-btn'>
+                        Upload
+                    </button>
+                </div>
+
+                <div className='note-container'>
+                </div>
+                
+            </div>
+
+            <div>
+                <Footer />
+            </div>
+        </div>
+    );
+};
+
+export default SummaryGenerator;
