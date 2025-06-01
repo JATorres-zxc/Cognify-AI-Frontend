e4155f87199f89647f62b0799755b249b4e6f851
Li~chan <137862600+ChiakiLi-chan@users.noreply.github.com>
2025-06-01 22:51:47 +0800

    added dropdown to Tools

--- src/components/common/Header/LandingPageHeader.jsx
+++ src/components/common/Header/LandingPageHeader.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import sensaiLogo from '../../../assets/images/sensai logo.png';
 import toolsIcon from '../../../assets/icons/tools.svg';
 import myStudyIcon from '../../../assets/icons/mystudy.svg';
@@ -7,7 +7,27 @@ import userIcon from '../../../assets/icons/user.svg';
 import './Header.css';
 import SearchBar from '../SearchBar/SearchBar';
 
-const LandingPageHeader = () => {
+const Header = () => {
+
+    const [isToolsOpen, setIsToolsOpen] = useState(false);
+    const toolsRef = useRef(null);
+
+    const toggleToolsDropdown = () => {
+        setIsToolsOpen(prev => !prev);
+    };
+
+    // Close dropdown if click outside
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+        if (toolsRef.current && !toolsRef.current.contains(event.target)) {
+            setIsToolsOpen(false);
+        }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, []);
+    
+
     return (
     <div className='header'>
         <div className='content'>
@@ -36,19 +56,32 @@ const LandingPageHeader = () => {
                     </div>
                 </div>
 
-                <div className='nav-container'>
+                <div
+                    className='nav-container'
+                    onClick={toggleToolsDropdown}
+                    ref={toolsRef}
+                >
                     <div className='nav-icon'>
                         <img src={toolsIcon} alt='Tools' className='icon' />
                     </div>
                     <div className='nav-text'>
                         Tools
                     </div>
+                    
+                    {isToolsOpen && (
+                        <div className='dropdown-menu'>
+                            <div className='dropdown-item'>Notes</div>
+                            <div className='dropdown-item'>Quizzes</div>
+                            <div className='dropdown-item'>Flashcards</div>
+                            <div className='dropdown-item'>Summaries</div>
+                        </div>
+                    )}
                 </div>
             </div>
 
             <div className='login-btn-container'>
                 <button className='btn'>
-                    Login
+                    Logout
                     <img src={userIcon} alt='Login' className='login-icon' />
                 </button>
             </div>
@@ -57,4 +90,4 @@ const LandingPageHeader = () => {
     );
 }
 
-export default LandingPageHeader;
+export default Header;
