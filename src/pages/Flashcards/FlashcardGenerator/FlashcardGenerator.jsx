1bf481cb75bbcdcb4c9369ba8d198919e1636a29
Li~chan <137862600+ChiakiLi-chan@users.noreply.github.com>
2025-06-02 00:45:02 +0800

    added flashcard generator page

---
+++ src/pages/Flashcards/FlashcardGenerator/FlashcardGenerator.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import Header from '../../../components/common/Header/Header';
+import Footer from '../../../components/common/Footer/Footer';
+import styles from './FlashcardGenerator.module.css';
+
+const FlashcardGenerator = () => {
+    return (
+        <div className={styles["home"]}>
+            <div>
+            <Header />
+            </div>
+            <div className={styles["main"]}>
+                <div className={styles["main-header"]}>
+                    <h1>Flashcard Generator</h1>
+                </div>
+                <div className={styles["line"]}></div>
+
+                <div className="btn-container">
+                    <button className={styles["upload-btn"]}>
+                        Upload
+                    </button>
+                </div>
+
+                <div className={styles["note-container"]}>
+                </div>
+                
+            </div>
+
+            <div>
+                <Footer />
+            </div>
+        </div>
+    );
+};
+
+export default FlashcardGenerator;
